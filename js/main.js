c7c544270c2ec694994d11c5e1b489ccc04541ed
Eradzh Mirzoev <eradzh.m@gmail.com>
2022-05-25 20:34:22 +0300

    fix bugs

--- js/main.js
+++ js/main.js
@@ -1,41 +1,34 @@
-function Clock() {
-    this.divClock = document.querySelector('.clock');
-    this.arr = [];
+function Clock(clockNode) {
+    this.clockNode = clockNode;
 
-    this.pushArr = function() {
-        let li = document.createElement('li');
-        this.arr.push(li);
-    };
-    
-    this.changeOption = function(e) {
-        e.target.classList.toggle('short-format');
-    };
-
-    this.divClock.addEventListener('click', this.changeOption.bind(this));
-
-    this.showTime = function() {
-        for (let i = 0; i < this.arr.length; i++) {
-            if (this.arr[i].className !== 'short-format') {
-                this.arr[i].innerHTML = new Date().toTimeString().split(' ')[0];
-            } else {
-                this.arr[i].innerHTML = new Date().toTimeString().split(' ')[0].substring(0,5);
-            }
-            this.divClock.append(this.arr[i]);
-        }
-    };
-
-    this.render = function() {
-        this.pushArr();
-        setInterval(()=> this.showTime(), 1000);
-    };
+    this.clockNode.addEventListener('click', (event) => {
+        event.target.classList.toggle('short-format');  
+    });
 }
 
-let clock = new Clock();
+Clock.prototype.startClock = function() {
+    let date = new Date().toTimeString().split(' ')[0];
+
+    if(this.clockNode.classList.contains('short-format')) {
+        date = new Date().toTimeString().split(' ')[0].substring(0,5);
+    }
+
+    this.clockNode.innerHTML = date;
+}
+
+Clock.prototype.render = function() {
+    setInterval(()=> this.startClock(), 1000);
+}
+
+let clock1 = new Clock(document.querySelector('.clock1'));
+let clock2 = new Clock(document.querySelector('.clock2'));
+let clock3 = new Clock(document.querySelector('.clock3'));
+
+clock1.render();
+clock2.render();
+clock3.render();
 
 
-clock.render();
-clock.render();
-clock.render();
 
 
 
